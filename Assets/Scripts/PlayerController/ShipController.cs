1e159946dccc95f95059d41c56e3b2bfd4fdecd5
Alonizi <Abdulaziz.Alonizi@gmail.com>
2025-02-18 11:50:07 +0300

    Fire + Rotation Dampening
    - Dampen the rotation of the ship's Z axis
    - Added Firing input

--- Assets/Scripts/PlayerController/ShipController.cs
+++ Assets/Scripts/PlayerController/ShipController.cs
@@ -4,18 +4,20 @@ using UnityEngine.InputSystem;
 
 public class ShipController : MonoBehaviour
 {
+    [SerializeField] private float BulletSpeed = 1f;
+    [SerializeField] private GameObject BulletPrefab;
     [SerializeField] private InputAction Thrusters;
     [SerializeField] private InputAction Fire;
     [SerializeField] private InputAction Aim;
     [SerializeField] private InputAction Boost;
-
+    [SerializeField] private float RotationDampening =.5f;
     private Rigidbody2D RigidComp;
-    
-    
+
     private void OnEnable()
     {
         Aim.Enable();
         Thrusters.Enable();
+        Fire.Enable();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,9 +30,13 @@ public class ShipController : MonoBehaviour
     void Update()
     {
         MoveShip();
+        FireBullet(RigidComp.rotation);
+    }
+    
+    private void FixedUpdate()
+    {
         AimShip();
     }
-
 
     private void MoveShip()
     {
@@ -43,25 +49,24 @@ public class ShipController : MonoBehaviour
     {
         if (Aim.IsInProgress())
         {
-            // var angleRad = (Mathf.Atan2(Aim.ReadValue<Vector2>().y, Aim.ReadValue<Vector2>().x)) - (90*Mathf.Deg2Rad);
-            //Mathf.SmoothDampAngle()
+            var velocity = 0f;
             var angle = (Mathf.Rad2Deg * Mathf.Atan2(Aim.ReadValue<Vector2>().y, Aim.ReadValue<Vector2>().x)) - 90;
-            //RigidComp.angularVelocity=angle;
-            RigidComp.SetRotation(angle);
-            //RigidComp.AddTorque(angleRad*RigidComp.inertia);
-            // var m_EulerAngleVelocity = new Vector3(0,0, angle);
-            // Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
-            //RigidComp.MoveRotation(RigidComp.rotation );
-            Debug.Log($"Aim : {angle}");
-            
-
+            var dampenedAngle = Mathf.SmoothDampAngle(RigidComp.rotation, angle, ref velocity, RotationDampening);
+            RigidComp.SetRotation(dampenedAngle);
+            Debug.Log($"Aim : {dampenedAngle}");
         }
-
     }
-
-    private void FixedUpdate()
+    
+    private void FireBullet(float angle)
     {
-        AimShip();
+        if (Fire.WasReleasedThisFrame())
+        {
+            
+            var bullet = Instantiate(BulletPrefab, transform.GetChild(0).position, Quaternion.Euler(0, 0, angle));
+            var direction = new Vector3(Mathf.Cos((angle+90)*Mathf.Deg2Rad), Mathf.Sin((angle+90)*Mathf.Deg2Rad), 0).normalized;
+            bullet.GetComponent<Rigidbody2D>().linearVelocity = BulletSpeed * direction;
+            
+        }
     }
 }
 
