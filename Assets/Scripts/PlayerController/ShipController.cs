0b0f1fa7285cc3a2985a64d618161ab8f7db29e2
Alonizi <Abdulaziz.Alonizi@gmail.com>
2025-02-24 15:14:30 +0300

    adjust scripts for homework

--- Assets/Scripts/PlayerController/ShipController.cs
+++ Assets/Scripts/PlayerController/ShipController.cs
@@ -1,19 +1,37 @@
-using System;
+//copyrights Abdulaziz Alonizi 2025
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// responsible for controlling the player's ship using an xbox controller
+/// </summary>
 public class ShipController : MonoBehaviour
 {
-    [SerializeField] private float BulletSpeed = 1f;
-    [SerializeField] private GameObject BulletPrefab;
-    [SerializeField] private InputAction Thrusters;
-    [SerializeField] private InputAction Fire;
+    [Header("Xbox Controller Mapping")]
+    
+    [Tooltip("Maps to Right Stick.")]
     [SerializeField] private InputAction Aim;
+    [Tooltip("Maps to Left Stick.")]
+    [SerializeField] private InputAction Thrusters;
+    [Tooltip("Maps to Right Bumper.")]
+    [SerializeField] private InputAction Fire;
+    [Tooltip("Maps to Right Trigger.")]
     [SerializeField] private InputAction Boost;
+    
+    [Header("Bullet")]
+    [SerializeField] private GameObject BulletPrefab;
+    
+    [Header("Ship's Settings")]
     [SerializeField] private float RotationDampening =.5f;
     [SerializeField] private float BoostMultiplier = 2f;
+    [SerializeField] private float BulletSpeed = 1f;
+    
     private Rigidbody2D RigidComp;
 
+    /// <summary>
+    /// enable all input sources 
+    /// </summary>
     private void OnEnable()
     {
         Aim.Enable();
@@ -21,13 +39,18 @@ public class ShipController : MonoBehaviour
         Fire.Enable();
         Boost.Enable();
     }
-
+    /// <summary>
+    /// cache rigidbody component on start 
+    /// </summary>
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         RigidComp = GetComponent<Rigidbody2D>();
     }
-
+    /// <summary>
+    /// call the functions responsible for moving , firing and boosting the ship
+    /// on each frame
+    /// </summary>
     // Update is called once per frame
     void Update()
     {
@@ -36,30 +59,45 @@ public class ShipController : MonoBehaviour
         SpeedBoost();
     }
     
+    /// <summary>
+    /// call the function responsible for aiming the ship 
+    /// </summary>
     private void FixedUpdate()
     {
         AimShip();
     }
-
+    
+    /// <summary>
+    /// control ship's linear velocity,
+    /// using vector2 values captured from controller's left stick
+    /// </summary>
     private void MoveShip()
     {
         //thrusters 
         RigidComp.linearVelocity = Thrusters.ReadValue<Vector2>();
         Debug.Log($"Thruster : {Thrusters.ReadValue<Vector2>()}");
     }
-
+    /// <summary>
+    /// control ship rotation 
+    /// using vector2 values captured from controller's right stick
+    /// </summary>
     private void AimShip()
     {
         if (Aim.IsInProgress())
         {
             var velocity = 0f;
             var angle = (Mathf.Rad2Deg * Mathf.Atan2(Aim.ReadValue<Vector2>().y, Aim.ReadValue<Vector2>().x)) - 90;
+            //add dampening to smooth out the rotation 
             var dampenedAngle = Mathf.SmoothDampAngle(RigidComp.rotation, angle, ref velocity, RotationDampening);
             RigidComp.SetRotation(dampenedAngle);
             Debug.Log($"Aim : {dampenedAngle}");
         }
     }
-    
+    /// <summary>
+    /// instantiate 'fire' a bullet
+    /// each time the controller's Right Bumper is pressed 
+    /// </summary>
+    /// <param name="angle"></param>
     private void FireBullet(float angle)
     {
         if (Fire.WasReleasedThisFrame())
@@ -69,10 +107,12 @@ public class ShipController : MonoBehaviour
             bullet.GetComponent<Rigidbody2D>().linearVelocity = BulletSpeed * direction;
         }
     }
-
+    /// <summary>
+    /// add force the ship 'Boost'
+    /// using float value captured from Controller's Right Trigger 
+    /// </summary>
     private void SpeedBoost()
     {
-        
         var boostValue = Boost.ReadValue<float>() * BoostMultiplier;
         Debug.Log($"Boost : {boostValue}");
         RigidComp.AddForce(boostValue*RigidComp.linearVelocity); 
--- Assets/Scripts/ShootingSystem/WeaponController.cs
+++ Assets/Scripts/ShootingSystem/WeaponController.cs
@@ -1,52 +1,60 @@
+// copyrights Abdulaziz Alonizi 2025
 using UnityEngine;
 
+/// <summary>
+/// Control Weapon Mechanism for Player's Ship
+/// </summary>
 public class WeaponController : MonoBehaviour
 {
     [SerializeField] private GameObject BulletPrefab;
-    
+    [SerializeField] private float BulletSpeed = 2f;
+    [SerializeField] private float FireRate = 0.2f;
+
     private Vector3 MouseDirection;
-    public float BulletSpeed = 2f;
-    public float FireRate = 0.2f;
     private float TimeCounter;
     private float ShipAxis ;
-    AudioManager audioManager; 
-
-    private void Start() {
-         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-    }
-
+    
+    /// <summary>
+    /// responsible for updating mouse position , ship rotation
+    /// and calling the Fire mechanism
+    /// </summary>
     void Update()
     {
-        TimeCounter += Time.deltaTime;
-        if (gameObject.tag == "Player")
-        {
             MouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             ShipAxis = AimShip();
             Fire(ShipAxis);
-        }
     }
     
+    /// <summary>
+    /// rotates the ship based on the direction of the mouse
+    /// </summary>
     private float AimShip()
     {
         var angle = (Mathf.Rad2Deg * Mathf.Atan2(MouseDirection.y, MouseDirection.x)) - 90;
-        //Debug.DrawRay(objectPosition,GetMouseDirection(),Color.cyan);
         transform.rotation = Quaternion.Euler(0, 0, angle);
         return angle;
     }
 
+    /// <summary>
+    /// Register user's input and fire a bullet at the specified angle
+    /// </summary>
+    /// <param name="angle"> the angle at which to fire the projectile</param>
     public void Fire(float angle)
     {
         if (Input.GetAxisRaw("Fire1") == 1  || Input.GetKey(KeyCode.Space))
         {
-            audioManager.PlayeSFX(audioManager.DieClip);
             TimeCounter += Time.deltaTime;
             if (TimeCounter > FireRate)
             {
-                var bullet = Instantiate(BulletPrefab, transform.GetChild(0).position, Quaternion.Euler(0, 0, angle));
-                bullet.GetComponent<Rigidbody2D>().linearVelocity = BulletSpeed * MouseDirection;
+                var bullet = Instantiate(
+                    BulletPrefab,
+                    transform.GetChild(0).position,
+                    Quaternion.Euler(0, 0, angle)
+                    );
+                Vector2 bulletDirection = MouseDirection.normalized;
+                bullet.GetComponent<Rigidbody2D>().linearVelocity = BulletSpeed * bulletDirection;
                 TimeCounter = 0; 
             }
         }
     }
-
 }
